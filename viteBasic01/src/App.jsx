ac602f0bba32ef01ce4b4f700845e9694d6c3380
harshil0218 <malaviyah.218@gmail.com>
2024-12-11 20:43:47 +0530

    project overview completed

---
+++ viteBasic01/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Coffee from './coffee'
+function App() {
+  return (
+    <> 
+      <Coffee/>
+      <h2>hey bro</h2> 
+    </>
+  )
+}
+
+export default App
